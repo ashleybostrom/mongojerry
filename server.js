710c22a65f60f1758244f26a13eafdc07b1f4602
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-19 21:41:13 -0700

    update serverjs w routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const routes = require('./routes');
 const db = require('./config/connection');
 
 const app = express();
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 
 db.once('open', () => {
