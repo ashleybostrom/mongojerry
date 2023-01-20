27b16ca47d6026e7c4c2cc58304062b98802a605
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-19 17:03:09 -0700

    config connection mongoose

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+// Connect to mongoose
+
+const { connect, connection } = require('mongoose');
+
+// Connect to MnongoDB Atlas for Heroku App or run local
+
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/your-db-name';
+
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Export
+module.exports = connection;
