f832d5eba87bdda20e22ea94d68b638242f75d75
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-19 22:42:38 -0700

    trying to fix routes still

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,7 +7,7 @@ const {
     deleteUser,
     addFriend,
     deleteFriend
-} = require('../..');
+} = require('../../controllers/userController');
 
 // /api/users get and post
 router.route('/').get(getUser).post(createUser);
