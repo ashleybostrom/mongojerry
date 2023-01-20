596ae2b8ff7aeb14f50d60db82aa6ab82b4a2e60
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-19 22:08:50 -0700

    index routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,13 @@
 // import express and routes
+const router = require('express').Router();
 
 // const express = require('express').Router;
 const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
 
 // create router
-Router.use('/users', userRoutes);
-Router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 // export router
 module.exports = router;
