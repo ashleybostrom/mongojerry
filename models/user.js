9440ec91f7526ccbf54597423a58a5c1b61145d1
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-19 17:27:10 -0700

    fixed closing symbol

--- models/user.js
+++ models/user.js
@@ -41,8 +41,7 @@ const userSchema = new Schema(
 
 userSchema.virtual('friendCount').get(function() {
     return this.friends.length;
-}
+});
 
-const User = model('User', userSchema);
-
+// const User = model('User', userSchema);
 module.exports = User;
