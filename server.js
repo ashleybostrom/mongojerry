f97b4993f79b1781661cc6fc16c1b91ed6f91444
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-19 20:48:49 -0700

    update serverjs

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`);
+   });
+});
