9e7d038104860980416378b8cab3d0a40919eba0
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-19 20:58:04 -0700

    thoughtroutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts get and post
+router.route('/').get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:id get one thought put and delete
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions post and delete
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
+
+//export
+module.exports = router;
