7e8a9d4fb630a053b6f263138b39cc0514428092
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-24 16:57:08 -0700

    fixed ids in userroutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+// import user controller
 const {
     getAllUsers,
     getUserById,
@@ -10,14 +11,14 @@ const {
     deleteFriend
 } = require('../../controllers/userController');
 
-// getAllUsers and createUser
+// get all users
 router.route('/').get(getAllUsers).post(createUser);
 
 // getUserById and updateUser and deleteUser
-router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
 
 // createFriend and deleteFriend
-router.route('/:id/friends/:friendId').post(createFriend).delete(deleteFriend);
+router.route('/:userId/friends/:friendId').post(createFriend).delete(deleteFriend);
 
 //export
 module.exports = router;
